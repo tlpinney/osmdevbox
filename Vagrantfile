a231f9cb0fc5c91c99a606ca48dc50492fce694b
Travis L Pinney <travis.pinney@gmail.com>
2012-02-19 11:12:18 -0500

    Checking in some code. Still incomplete.

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,12 @@ Vagrant::Config.run do |config|
 
   config.vm.box = "lucid32"
 
+config.vm.customize [
+  "modifyvm", :id,
+  "--name", "OpenStreetMap Dev VM v0.1",
+  "--memory", "2048"
+] 
+
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "manifests"
     puppet.manifest_file  = "base.pp"
--- manifests/base.pp
+++ manifests/base.pp
@@ -8,3 +8,86 @@
    content => "Welcome to your OpenStreetMap Dev Box v0.1!"
  }
 
+ 
+
+ package { "git-core":
+  ensure => installed,
+ }
+
+ package { "vim":
+  ensure => installed,
+ }
+
+ package { "postgresql-contrib":
+  ensure => installed,
+ }
+
+
+ package { "ruby":
+  ensure => installed,
+ }
+
+ package { "rdoc":
+  ensure => installed,
+ }
+
+ package { "ri":
+  ensure => installed,
+ }
+
+ package { "libpq-dev":
+  ensure => installed,
+ }
+
+ package { "libxml2-dev":
+ ensure => installed,
+ }
+
+ package { "libxslt1-dev":
+ ensure => installed,
+ }
+
+ package { "ruby-dev":
+ ensure => installed,
+ }
+
+ package { "apache2-dev":
+ ensure => installed,
+ }
+
+ package { "libmagick9-dev":
+ ensure => installed,
+ }
+
+package { "build-essential":
+ ensure => installed,
+ }
+
+package { "libopenssl-ruby":
+ ensure => installed,
+ }
+
+package { "subversion":
+ ensure => installed,
+ }
+
+package { "apache2":
+ ensure => installed,
+ }
+
+
+package { "postgresql": 
+ ensure => installed,
+}
+
+
+
+
+
+exec { "Create puppet Git repo":
+  cwd => "/home/vagrant",
+  user => "vagrant",
+  command => "/usr/bin/git clone https://github.com/openstreetmap/openstreetmap-website.git",
+  creates => "/home/vagrant/openstreetmap-website",
+  require => [Package["git-core"]],
+} 
